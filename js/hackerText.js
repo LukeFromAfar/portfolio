0c010abe342ef52a6e5ca36b656a6a26480df2ae
Lukasz <lukasz.gamvik@gmail.com>
2024-09-02 20:11:37 +0200

    added better font

--- js/hackerText.js
+++ js/hackerText.js
@@ -1,31 +1,64 @@
+// const letters = "abcdefghijklmnopqrstuvwxyz";
+
+
+// // ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+// let interval = null;
+
+// document.getElementsByClassName(".heroTitle").onmouseover = event => {  
+//   let iteration = 0;
+  
+//   clearInterval(interval);
+  
+//   interval = setInterval(() => {
+//     event.target.innerText = event.target.innerText
+//       .split("")
+//       .map((letter, index) => {
+//         if(index < iteration) {
+//           return event.target.dataset.value[index];
+//         }
+      
+//         return letters[Math.floor(Math.random() * 26)]
+//       })
+//       .join("");
+    
+//     if(iteration >= event.target.dataset.value.length){ 
+//       clearInterval(interval);
+//     }
+    
+//     iteration += 1 / 3;
+//   }, 30);
+// }
+
 const letters = "abcdefghijklmnopqrstuvwxyz";
 
+const elements = document.getElementsByClassName("heroTitle");
 
-// ABCDEFGHIJKLMNOPQRSTUVWXYZ
-
-let interval = null;
-
-document.querySelector(".heroTitle").onmouseover = event => {  
-  let iteration = 0;
+Array.from(elements).forEach(element => {
+  let interval = null;  // Create a separate interval variable for each element
   
-  clearInterval(interval);
-  
-  interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
-      .split("")
-      .map((letter, index) => {
-        if(index < iteration) {
-          return event.target.dataset.value[index];
-        }
+  element.onmouseover = event => {
+    let iteration = 0;
+    
+    clearInterval(interval);
+    
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+          
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
       
-        return letters[Math.floor(Math.random() * 26)]
-      })
-      .join("");
-    
-    if(iteration >= event.target.dataset.value.length){ 
-      clearInterval(interval);
-    }
-    
-    iteration += 1 / 3;
-  }, 30);
-}
+      if (iteration >= event.target.dataset.value.length) { 
+        clearInterval(interval);
+      }
+      
+      iteration += 1 / 3;
+    }, 30);
+  };
+});
